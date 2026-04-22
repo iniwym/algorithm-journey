e42933636c5730539b47467423da2666c45b45cb
algorithmzuo <algorithmzuo@163.com>
2026-04-22 17:14:28 +0800

    modify code

---
+++ src/class197/Code02_Mafia1.java
@@ -0,0 +1,10 @@
+package class197;
+
+// 黑手党，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF587D
+// 测试链接 : https://codeforces.com/problemset/problem/587/D
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+public class Code02_Mafia1 {
+
+}
