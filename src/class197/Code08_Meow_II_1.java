e098e812ebfdcddc0880587a8a3b805c0fa64046
algorithmzuo <algorithmzuo@163.com>
2026-04-26 23:11:05 +0800

    modify code

---
+++ src/class197/Code08_Meow_II_1.java
@@ -0,0 +1,314 @@
+package class197;
+
+// 喵了个喵 II，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P9139
+// 测试链接 : https://loj.ac/p/6871
+// 提交以下的code，提交时请把类名改成"Main"
+// 本题给的使用空间对于java来说不够，所以java实现无法通过测试
+// 想通过用C++实现，本节课Code08_Meow_II_2文件就是C++的实现
+// 两个版本的逻辑完全一样，C++版本可以通过所有测试
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Code08_Meow_II_1 {
+
+	public static int MAXV = 50001;
+	public static int MAXN = 200001;
+	public static int MAXT = 10000001;
+	public static int MAXE = 50000001;
+	public static int v, n;
+
+	// 值v、位置i
+	public static int[][] arr = new int[MAXN][2];
+
+	// 左l、右r、决定编号x
+	public static int[][] groupArr = new int[MAXN][3];
+	public static int cnta;
+
+	public static int[] head = new int[MAXT];
+	public static int[] nxt = new int[MAXE];
+	public static int[] to = new int[MAXE];
+	public static int cntg;
+
+	public static int[] dfn = new int[MAXT];
+	public static int[] low = new int[MAXT];
+	public static int cntd;
+
+	public static int[] sta = new int[MAXT];
+	public static int top;
+
+	public static int[] belong = new int[MAXT];
+	public static int sccCnt;
+
+	public static int[] rootOut = new int[MAXN];
+	public static int[] rootIn = new int[MAXN];
+	public static int[] ls = new int[MAXT];
+	public static int[] rs = new int[MAXT];
+	public static int cntt;
+
+	public static int[] ans = new int[MAXN];
+
+	public static void addPair(int l, int r, int x) {
+		groupArr[++cnta][0] = l;
+		groupArr[cnta][1] = r;
+		groupArr[cnta][2] = x;
+	}
+
+	public static void addEdge(int u, int v) {
+		nxt[++cntg] = head[u];
+		to[cntg] = v;
+		head[u] = cntg;
+	}
+
+	public static void tarjan(int u) {
+		dfn[u] = low[u] = ++cntd;
+		sta[++top] = u;
+		for (int e = head[u]; e > 0; e = nxt[e]) {
+			int v = to[e];
+			if (dfn[v] == 0) {
+				tarjan(v);
+				low[u] = Math.min(low[u], low[v]);
+			} else {
+				if (belong[v] == 0) {
+					low[u] = Math.min(low[u], dfn[v]);
+				}
+			}
+		}
+		if (dfn[u] == low[u]) {
+			sccCnt++;
+			int pop;
+			do {
+				pop = sta[top--];
+				belong[pop] = sccCnt;
+			} while (pop != u);
+		}
+	}
+
+	public static int buildOut(int l, int r) {
+		int rt = ++cntt;
+		if (l < r) {
+			int mid = (l + r) >> 1;
+			ls[rt] = buildOut(l, mid);
+			rs[rt] = buildOut(mid + 1, r);
+			addEdge(ls[rt], rt);
+			addEdge(rs[rt], rt);
+		}
+		return rt;
+	}
+
+	public static int buildIn(int l, int r) {
+		int rt = ++cntt;
+		if (l < r) {
+			int mid = (l + r) >> 1;
+			ls[rt] = buildIn(l, mid);
+			rs[rt] = buildIn(mid + 1, r);
+			addEdge(rt, ls[rt]);
+			addEdge(rt, rs[rt]);
+		}
+		return rt;
+	}
+
+	public static int addOut(int jobi, int jobx, int l, int r, int i) {
+		int rt = ++cntt;
+		ls[rt] = ls[i];
+		rs[rt] = rs[i];
+		addEdge(i, rt);
+		if (l == r) {
+			addEdge(jobx, rt);
+		} else {
+			int mid = (l + r) >> 1;
+			if (jobi <= mid) {
+				ls[rt] = addOut(jobi, jobx, l, mid, ls[rt]);
+				addEdge(ls[rt], rt);
+			} else {
+				rs[rt] = addOut(jobi, jobx, mid + 1, r, rs[rt]);
+				addEdge(rs[rt], rt);
+			}
+		}
+		return rt;
+	}
+
+	public static int addIn(int jobi, int jobx, int l, int r, int i) {
+		int rt = ++cntt;
+		ls[rt] = ls[i];
+		rs[rt] = rs[i];
+		addEdge(rt, i);
+		if (l == r) {
+			addEdge(rt, jobx);
+		} else {
+			int mid = (l + r) >> 1;
+			if (jobi <= mid) {
+				ls[rt] = addIn(jobi, jobx, l, mid, ls[rt]);
+				addEdge(rt, ls[rt]);
+			} else {
+				rs[rt] = addIn(jobi, jobx, mid + 1, r, rs[rt]);
+				addEdge(rt, rs[rt]);
+			}
+		}
+		return rt;
+	}
+
+	public static void xToRange(int jobx, int jobl, int jobr, int l, int r, int i) {
+		if (jobl <= l && r <= jobr) {
+			addEdge(jobx, i);
+		} else {
+			int mid = (l + r) >> 1;
+			if (jobl <= mid) {
+				xToRange(jobx, jobl, jobr, l, mid, ls[i]);
+			}
+			if (jobr > mid) {
+				xToRange(jobx, jobl, jobr, mid + 1, r, rs[i]);
+			}
+		}
+	}
+
+	public static void rangeToX(int jobl, int jobr, int jobx, int l, int r, int i) {
+		if (jobl <= l && r <= jobr) {
+			addEdge(i, jobx);
+		} else {
+			int mid = (l + r) >> 1;
+			if (jobl <= mid) {
+				rangeToX(jobl, jobr, jobx, l, mid, ls[i]);
+			}
+			if (jobr > mid) {
+				rangeToX(jobl, jobr, jobx, mid + 1, r, rs[i]);
+			}
+		}
+	}
+
+	public static int other(int x) {
+		return x <= v ? x + v : x - v;
+	}
+
+	public static void buildGraph() {
+		Arrays.sort(groupArr, 1, cnta + 1, (a, b) -> a[1] - b[1]);
+		cntt = v << 1;
+		rootOut[0] = buildOut(1, n);
+		rootIn[0] = buildIn(1, n);
+		for (int i = 1; i <= cnta; i++) {
+			int l = groupArr[i][0];
+			int r = groupArr[i][1];
+			int x = groupArr[i][2];
+			xToRange(x, l, r, 1, n, rootIn[i - 1]);
+			rangeToX(l, r, other(x), 1, n, rootOut[i - 1]);
+			rootIn[i] = addIn(l, other(x), 1, n, rootIn[i - 1]);
+			rootOut[i] = addOut(l, x, 1, n, rootOut[i - 1]);
+		}
+	}
+
+	public static boolean compute() {
+		Arrays.sort(arr, 1, n + 1, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+		for (int i = 1; i <= n; i += 4) {
+			int x = arr[i][0];
+			int a = arr[i][1];
+			int b = arr[i + 1][1];
+			int c = arr[i + 2][1];
+			int d = arr[i + 3][1];
+			addPair(a, b, x);
+			addPair(c, d, x);
+			addPair(a, c, x + v);
+			addPair(b, d, x + v);
+		}
+		buildGraph();
+		for (int i = 1; i <= v << 1; i++) {
+			if (dfn[i] == 0) {
+				tarjan(i);
+			}
+		}
+		boolean check = true;
+		for (int i = 1; i <= v; i++) {
+			if (belong[i] == belong[i + v]) {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			for (int i = 1; i <= n; i += 4) {
+				int x = arr[i][0];
+				int a = arr[i][1];
+				int b = arr[i + 1][1];
+				int c = arr[i + 2][1];
+				int d = arr[i + 3][1];
+				if (belong[x] < belong[x + v]) {
+					ans[a] = 0;
+					ans[b] = 1;
+					ans[c] = 0;
+					ans[d] = 1;
+				} else {
+					ans[a] = 0;
+					ans[b] = 0;
+					ans[c] = 1;
+					ans[d] = 1;
+				}
+			}
+		}
+		return check;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		v = in.nextInt();
+		n = v << 2;
+		for (int i = 1; i <= n; i++) {
+			arr[i][0] = in.nextInt();
+			arr[i][1] = i;
+		}
+		boolean check = compute();
+		if (check) {
+			out.println("Yes");
+			for (int i = 1; i <= n; i++) {
+				out.print(ans[i]);
+			}
+		} else {
+			out.println("No");
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class197/Code08_Meow_II_2.java
@@ -0,0 +1,275 @@
+package class197;
+
+// 喵了个喵 II，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P9139
+// 测试链接 : https://loj.ac/p/6871
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Node {
+//    int v, i;
+//};
+//
+//bool NodeCmp(Node a, Node b) {
+//    if (a.v != b.v) {
+//        return a.v < b.v;
+//    }
+//    return a.i < b.i;
+//}
+//
+//struct Group {
+//    int l, r, x;
+//};
+//
+//bool GroupCmp(Group a, Group b) {
+//    return a.r < b.r;
+//}
+//
+//const int MAXV = 50001;
+//const int MAXN = 200001;
+//const int MAXT = 10000001;
+//const int MAXE = 50000001;
+//int v, n;
+//
+//Node arr[MAXN];
+//Group groupArr[MAXN];
+//int cnta;
+//
+//int head[MAXT];
+//int nxt[MAXE];
+//int to[MAXE];
+//int cntg;
+//
+//int dfn[MAXT];
+//int low[MAXT];
+//int cntd;
+//
+//int sta[MAXT];
+//int top;
+//
+//int belong[MAXT];
+//int sccCnt;
+//
+//int rootOut[MAXN];
+//int rootIn[MAXN];
+//int ls[MAXT];
+//int rs[MAXT];
+//int cntt;
+//
+//int ans[MAXN];
+//
+//void addEdge(int u, int v) {
+//    nxt[++cntg] = head[u];
+//    to[cntg] = v;
+//    head[u] = cntg;
+//}
+//
+//void tarjan(int u) {
+//    dfn[u] = low[u] = ++cntd;
+//    sta[++top] = u;
+//    for (int e = head[u]; e > 0; e = nxt[e]) {
+//        int v = to[e];
+//        if (dfn[v] == 0) {
+//            tarjan(v);
+//            low[u] = min(low[u], low[v]);
+//        } else {
+//            if (belong[v] == 0) {
+//                low[u] = min(low[u], dfn[v]);
+//            }
+//        }
+//    }
+//    if (dfn[u] == low[u]) {
+//        sccCnt++;
+//        int pop;
+//        do {
+//            pop = sta[top--];
+//            belong[pop] = sccCnt;
+//        } while (pop != u);
+//    }
+//}
+//
+//int buildOut(int l, int r) {
+//    int rt = ++cntt;
+//    if (l < r) {
+//        int mid = (l + r) >> 1;
+//        ls[rt] = buildOut(l, mid);
+//        rs[rt] = buildOut(mid + 1, r);
+//        addEdge(ls[rt], rt);
+//        addEdge(rs[rt], rt);
+//    }
+//    return rt;
+//}
+//
+//int buildIn(int l, int r) {
+//    int rt = ++cntt;
+//    if (l < r) {
+//        int mid = (l + r) >> 1;
+//        ls[rt] = buildIn(l, mid);
+//        rs[rt] = buildIn(mid + 1, r);
+//        addEdge(rt, ls[rt]);
+//        addEdge(rt, rs[rt]);
+//    }
+//    return rt;
+//}
+//
+//int addOut(int jobi, int jobx, int l, int r, int i) {
+//    int rt = ++cntt;
+//    ls[rt] = ls[i];
+//    rs[rt] = rs[i];
+//    addEdge(i, rt);
+//    if (l == r) {
+//        addEdge(jobx, rt);
+//    } else {
+//        int mid = (l + r) >> 1;
+//        if (jobi <= mid) {
+//            ls[rt] = addOut(jobi, jobx, l, mid, ls[rt]);
+//            addEdge(ls[rt], rt);
+//        } else {
+//            rs[rt] = addOut(jobi, jobx, mid + 1, r, rs[rt]);
+//            addEdge(rs[rt], rt);
+//        }
+//    }
+//    return rt;
+//}
+//
+//int addIn(int jobi, int jobx, int l, int r, int i) {
+//    int rt = ++cntt;
+//    ls[rt] = ls[i];
+//    rs[rt] = rs[i];
+//    addEdge(rt, i);
+//    if (l == r) {
+//        addEdge(rt, jobx);
+//    } else {
+//        int mid = (l + r) >> 1;
+//        if (jobi <= mid) {
+//            ls[rt] = addIn(jobi, jobx, l, mid, ls[rt]);
+//            addEdge(rt, ls[rt]);
+//        } else {
+//            rs[rt] = addIn(jobi, jobx, mid + 1, r, rs[rt]);
+//            addEdge(rt, rs[rt]);
+//        }
+//    }
+//    return rt;
+//}
+//
+//void xToRange(int jobx, int jobl, int jobr, int l, int r, int i) {
+//    if (jobl <= l && r <= jobr) {
+//        addEdge(jobx, i);
+//    } else {
+//        int mid = (l + r) >> 1;
+//        if (jobl <= mid) {
+//            xToRange(jobx, jobl, jobr, l, mid, ls[i]);
+//        }
+//        if (jobr > mid) {
+//            xToRange(jobx, jobl, jobr, mid + 1, r, rs[i]);
+//        }
+//    }
+//}
+//
+//void rangeToX(int jobl, int jobr, int jobx, int l, int r, int i) {
+//    if (jobl <= l && r <= jobr) {
+//        addEdge(i, jobx);
+//    } else {
+//        int mid = (l + r) >> 1;
+//        if (jobl <= mid) {
+//            rangeToX(jobl, jobr, jobx, l, mid, ls[i]);
+//        }
+//        if (jobr > mid) {
+//            rangeToX(jobl, jobr, jobx, mid + 1, r, rs[i]);
+//        }
+//    }
+//}
+//
+//int other(int x) {
+//    return x <= v ? x + v : x - v;
+//}
+//
+//void buildGraph() {
+//    sort(groupArr + 1, groupArr + cnta + 1, GroupCmp);
+//    cntt = v << 1;
+//    rootOut[0] = buildOut(1, n);
+//    rootIn[0] = buildIn(1, n);
+//    for (int i = 1; i <= cnta; i++) {
+//        int l = groupArr[i].l;
+//        int r = groupArr[i].r;
+//        int x = groupArr[i].x;
+//        xToRange(x, l, r, 1, n, rootIn[i - 1]);
+//        rangeToX(l, r, other(x), 1, n, rootOut[i - 1]);
+//        rootIn[i] = addIn(l, other(x), 1, n, rootIn[i - 1]);
+//        rootOut[i] = addOut(l, x, 1, n, rootOut[i - 1]);
+//    }
+//}
+//
+//bool compute() {
+//    sort(arr + 1, arr + n + 1, NodeCmp);
+//    for (int i = 1; i <= n; i += 4) {
+//        int x = arr[i].v;
+//        int a = arr[i].i;
+//        int b = arr[i + 1].i;
+//        int c = arr[i + 2].i;
+//        int d = arr[i + 3].i;
+//        groupArr[++cnta] = { a, b, x };
+//        groupArr[++cnta] = { c, d, x };
+//        groupArr[++cnta] = { a, c, x + v };
+//        groupArr[++cnta] = { b, d, x + v };
+//    }
+//    buildGraph();
+//    for (int i = 1; i <= v << 1; i++) {
+//        if (dfn[i] == 0) {
+//            tarjan(i);
+//        }
+//    }
+//    bool check = true;
+//    for (int i = 1; i <= v; i++) {
+//        if (belong[i] == belong[i + v]) {
+//            check = false;
+//            break;
+//        }
+//    }
+//    if (check) {
+//        for (int i = 1; i <= n; i += 4) {
+//            int x = arr[i].v;
+//            int a = arr[i].i;
+//            int b = arr[i + 1].i;
+//            int c = arr[i + 2].i;
+//            int d = arr[i + 3].i;
+//            if (belong[x] < belong[x + v]) {
+//                ans[a] = 0;
+//                ans[b] = 1;
+//                ans[c] = 0;
+//                ans[d] = 1;
+//            } else {
+//                ans[a] = 0;
+//                ans[b] = 0;
+//                ans[c] = 1;
+//                ans[d] = 1;
+//            }
+//        }
+//    }
+//    return check;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> v;
+//    n = v << 2;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i].v;
+//        arr[i].i = i;
+//    }
+//    bool check = compute();
+//    if (check) {
+//        cout << "Yes" << "\n";
+//        for (int i = 1; i <= n; i++) {
+//            cout << ans[i];
+//        }
+//    } else {
+//        cout << "No" << "\n";
+//    }
+//    return 0;
+//}
